7570f8c81a5603a54cedb34644e666a999058902
Jéssika <jessikafujimura@gmail.com>
2022-12-02 22:22:30 -0300

    implementacao do padrao chain of responsibility

--- DesignerPattern1_Loja/src/br/com/alura/loja/DesignerPattern1_Loja.java
+++ DesignerPattern1_Loja/src/br/com/alura/loja/DesignerPattern1_Loja.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
@@ -8,8 +9,23 @@ import java.math.BigDecimal;
 
 public class DesignerPattern1_Loja {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+    testeCalculadoraDescontos();
+    }
+
+    public static void testeCalculadoraImpostos(){
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
         System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
     }
+
+    public static void testeCalculadoraDescontos(){
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+        System.out.println(calculadoraDeDescontos.calcular(primeiro));
+        System.out.println(calculadoraDeDescontos.calcular(segundo));
+
+    }
+
 }
